cd92ecda079a3d184c5ec7a1e14ed88f162c5adb
kaijunge <kaijunge21@gmail.com>
2019-08-02 19:19:23 +0100

    Added current sensing functions + initial gripping cases for the arduino code

--- Arduino/Gripper_2_handler/Gripper_2_handler.ino
+++ Arduino/Gripper_2_handler/Gripper_2_handler.ino
@@ -17,7 +17,15 @@
 #define fastArmPin A2
 
 #define fastArmLimit 180
-#define slowArmLimit 885
+#define slowArmLimit 880
+#define closingLimit 165 //165
+
+#define potTolerance 10
+
+bool fastArmStatus = false;
+bool slowArmStatus = false;
+
+#define ARMS_TOGETHER 3
 
 void setup() {
   // put your setup code here, to run once:
@@ -30,11 +38,128 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
-  Serial.println(armPos_raw(slowArm));
-  arm_to_base(slowArm);
-  delay(999999999);
+  initArms();
+
+  //gripObjects(2);
+  /*
+  int maxval = 0;
+  moveArm(slowArm, CLOSE, 100);
+  moveArm(fastArm, CLOSE, 100);
+  while(errorCheck() == 0) {
+    int value = readCurrent();
+    Serial.println(value);
+
+    if(value > maxval) {
+      maxval = value;
+    }
+  }
+  Serial.print("max:  "); Serial.println(maxval);
+  moveArm(slowArm, STOP, 0);
+  moveArm(fastArm, STOP, 0);
+  */
+
+  delay(999999);
+}
+
+void gripObjects(int mode) {
+
+  switch(mode) {
+    case 0:
+      Serial.print("Gripping Objects, Fast Arm: "); Serial.println(mode);
+      
+      moveArm(fastArm, CLOSE, 100);
+      delay(100);
+      while((readCurrent() > 20 || readCurrent() < 5) && errorCheck() == 0) {}
+
+      while(true) {
+        if(readCurrent() > 40) {
+          Serial.println("Current Detected");
+          break;
+        }
+
+        if(errorCheck() != 0) {
+          Serial.println("Error Detected");
+          break;
+        }
+      }
+
+      moveArm(fastArm, STOP, 0);
+
+      break;
+    case 1:
+
+      break;
+    case 2:
+      Serial.print("Gripping Objects, Fast Arm: "); Serial.println(mode);
+      
+      moveArm(fastArm, CLOSE, 100);
+      moveArm(slowArm, CLOSE, 100);
+      delay(100);
+      while((readCurrent() > 50 || readCurrent() < 5) && errorCheck() == 0) {}
+
+      while(true) {
+
+        Serial.println(readCurrent());
+        if(readCurrent() > 80) {
+          Serial.println("Current Detected");
+          break;
+        }
+
+        if(errorCheck() != 0) {
+          Serial.println("Error Detected");
+          break;
+        }
+      }
+
+      moveArm(fastArm, STOP, 0);
+      moveArm(slowArm, STOP, 0);
+      break;
+    case 3:
+
+      break;
+    default:
+      Serial.println("No gripping style specifified");
+      break;
+  }
+  
+}
+
+void initArms() {
+  Serial.println("Initiating Arms");
+  both_arms_to_base();
+
+
+// UNNEEDED MOVEMENTS
+/*
+  if(errorCheck() == 0) {
+      Serial.println("Assuming both arms at base!");
+      moveArm(slowArm, CLOSE, 100);
+      moveArm(fastArm, CLOSE, 100);
+      delay(500);
+    
+      moveArm(slowArm, OPEN, 100);
+      moveArm(fastArm, OPEN, 100);
+    
+      while(fastArmStatus == true || slowArmStatus == true) {
+        if(armPos_raw(fastArm) < fastArmLimit) {
+          moveArm(fastArm, STOP, 0);
+        }
+        
+        if(armPos_raw(slowArm) > slowArmLimit) {
+          moveArm(slowArm, STOP, 0);
+        } 
+      }
+  }
+  */
+  Serial.print("Error Code:  "); Serial.println(errorCheck());
+  Serial.print("Fast arm:  "); Serial.println(armPos_raw(fastArm));
+  Serial.print("Slow arm:  "); Serial.println(armPos_raw(slowArm));
+  Serial.println("Initiation Complete\n\n");
+}
+
+float readCurrent() {
+  return analogRead(A0);
 }
 
 int currpos(bool whichArm) {
@@ -43,17 +168,59 @@ int currpos(bool whichArm) {
 
 void arm_to_base(bool whichArm) {
 
-  motor(whichArm, OPEN, 100);
+  moveArm(whichArm, OPEN, 100);
 
   if(whichArm == fastArm) {
-    while(armPos_raw(whichArm) > fastArmLimit) {}
+    while(armPos_raw(whichArm) > fastArmLimit + potTolerance) {}
   }
   else {
-    while(armPos_raw(whichArm) < slowArmLimit) {}
+    while(armPos_raw(whichArm) < slowArmLimit - potTolerance) {}
   }
-  motor(whichArm, STOP, 0);
+  moveArm(whichArm, STOP, 0);
 }
 
+void both_arms_to_base() {
+
+  if(errorCheck() == ARMS_TOGETHER) {
+
+    bool whichArm;
+    if(abs(armPos_raw(slowArm) - slowArmLimit) > abs(armPos_raw(fastArm) - fastArmLimit)) {
+      whichArm = slowArm;
+    }
+    else {
+      whichArm = fastArm; 
+    }
+
+    moveArm(whichArm, OPEN, 100);
+    delay(500);
+  }
+  
+  moveArm(slowArm, OPEN, 100);
+  moveArm(fastArm, OPEN, 100);
+
+  while(fastArmStatus == true || slowArmStatus == true) {
+
+    if(armPos_raw(fastArm) < fastArmLimit + potTolerance) {
+      moveArm(fastArm, STOP, 0);
+    }
+    
+    if(armPos_raw(slowArm) > slowArmLimit - potTolerance) {
+      moveArm(slowArm, STOP, 0);
+    }
+
+    if(errorCheck() != 0) {
+      Serial.print("ERROR OCCURED CHECK SYSTEM!!    Error: ");
+      Serial.println(errorCheck());
+      moveArm(fastArm, STOP, 0);
+      moveArm(slowArm, STOP, 0);
+    }  
+  }
+
+  if(errorCheck() == 0) {
+    Serial.println("Both arms back to base");
+  }
+ 
+}
 
 int armPos_raw(bool whichArm){
   if(whichArm == slowArm) {
@@ -64,14 +231,39 @@ int armPos_raw(bool whichArm){
   }
 }
 
+int errorCheck() {
+  int error = 0;
+  
+  // IF ARM IS BEYOND THEIR LIMITS
+  if(armPos_raw(fastArm) < fastArmLimit - potTolerance) {
+     error = 1;
+  }
 
-void motor(bool motor_select, int dir, float percentage_power) {
+  if(armPos_raw(slowArm) > slowArmLimit + potTolerance) {
+    error = 2;
+  }
+  
+  // IF ARMS ARE COLLIDING TO EACH OTHER
+  if(armPos_raw(slowArm) - armPos_raw(fastArm) < closingLimit) {
+    error = 3;
+  }
+
+  return error;
+}
+
+int currentSense() {
+  return 0;
+}
+
+
+void moveArm(bool whichArm, int dir, float percentage_power) {
 
     int power = (int)percentage_power*255/100;
 
-    if(motor_select == slowArm) {
+    if(whichArm == slowArm) {
           if(dir == STOP) {
             digitalWrite(MA_1, LOW); digitalWrite(MA_2, LOW); analogWrite(MA_PWM, 0);
+            slowArmStatus = false;
           }
           else{
             if(dir==OPEN){
@@ -80,11 +272,14 @@ void motor(bool motor_select, int dir, float percentage_power) {
             else{
               digitalWrite(MA_1, LOW); digitalWrite(MA_2, HIGH); analogWrite(MA_PWM, power);
             }
+            slowArmStatus = true;
+
           }
     }
     else {
           if(dir == STOP) {
             digitalWrite(MB_1, LOW); digitalWrite(MB_2, LOW); analogWrite(MB_PWM, 0);
+            fastArmStatus = false;
           }
           else{
             if(dir==CLOSE){
@@ -93,6 +288,9 @@ void motor(bool motor_select, int dir, float percentage_power) {
             else{
               digitalWrite(MB_1, LOW); digitalWrite(MB_2, HIGH); analogWrite(MB_PWM, power);
             }
+            fastArmStatus = true;
           }
     }
+
+    //Serial.print(fastArmStatus); Serial.println(slowArmStatus);
 }
